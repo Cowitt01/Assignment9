b430abbdafb2baf5774e66fdfa4e7de7dae226fb
Cory Witt <cpwitt@mail.mccneb.edu>
2025-07-07 18:57:20 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*
+Cory Witt
+script.js
+INFO 1579
+Shaw
+07/07/2025
+*/
+
+"use strict";
+// This script handles the weather information retrieval and display for a user's current location.
+
 $(document).ready( () => {
     startClock();
     
@@ -120,63 +131,3 @@ $(document).ready( () => {
                     })
     }
 });
-
-class WeatherForecast {
-    constructor(d, t, tM, tX) {
-        console.log(d, t, tM, tX);
-        this.date = d;
-        this.temp = t;
-        this.tempMin = tM;
-        this.tempMax = tX;
-    }
-
-    getDate() {
-        return new Date(this.date);
-    }
-
-    getTemp() {
-        return this.temp;
-    }
-
-    getMaxTemp() {
-        return this.tempMax;
-    }
-
-    getMinTemp() {
-        return this.tempMin;
-    }
-
-    getDayString() {
-        let d = new Date(this.date);
-        let day = d.getDay();
-        let date = d.getDate();
-        let mon = d.getMonth()  + 1;
-        let dayName = '';
-        switch(day) {
-            case 0:
-                dayName = 'Sun';
-            break;
-            case 1:
-                dayName = 'Mon';
-                break;
-            case 2:
-                dayName = 'Tue';
-                break;
-            case 3:
-                dayName = 'Wed';
-                break;
-            case 4:
-                dayName = 'Thu';
-                break;
-            case 5:
-                dayName = 'Fri';
-                break;
-            case 6:
-                dayName = 'Sat';
-                break;
-        }
-
-        let s = `${dayName}, ${mon}/${date}`;
-        return s;
-    }
-}
